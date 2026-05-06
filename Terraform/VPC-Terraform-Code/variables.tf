48ac111af18735830573637bed1e5fdc60a6d5cd
abduleic <abdul.muqtadeer@einfochips.com>
2026-05-06 19:33:49 +0530

    Create variables.tf

---
+++ Terraform/VPC-Terraform-Code/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {}
+variable "vpc_id" {}
+variable "internet_gateway_id" {}
+variable "public_route_table_id" {}
+variable "nat_gateway_id" {}
+variable "public_subnet_cidrs" {}
+variable "private_subnet_cidrs" {}
