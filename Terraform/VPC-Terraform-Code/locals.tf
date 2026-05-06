babde0f93fc093e185376da3f2eff36ad622f5e0
abduleic <abdul.muqtadeer@einfochips.com>
2026-05-06 19:32:27 +0530

    Create locals.tf

---
+++ Terraform/VPC-Terraform-Code/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  common_tags = {
+    Owner          = "abdul.muqtadeer@einfochips.com"
+    DM             = "Sachin.Koshti@einfochips.com"
+    Department     = "PES"
+    "Project Name" = "Devops Bootcamp"
+    "End Date"     = "26-05-2026"
+    BU             = "Intelligent Automation"
+  }
+}
